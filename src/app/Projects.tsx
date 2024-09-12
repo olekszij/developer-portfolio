65eac1acf235dbc7f3c395e45b051938be8d8d02
Olekszij <olekszijk@gmail.com>
2024-09-12 02:27:44 +0200

    add marvel

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -6,7 +6,7 @@ import { BiLinkExternal } from 'react-icons/bi';
 
 const projects = [
     { name: "Vinted replica", description: "frontend/backend", image: "/images/project1.jpg", link: "https://vintversion.netlify.app/" },
-    { name: "Project 2", description: "Description of project 2", image: "/images/project2.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
+    { name: "Marvel Comics Database", description: "Frontend React", image: "/images/project2.jpg", link: "https://jenemarrrrvel.netlify.app/" },
     { name: "Deliveroo replica", description: "Frontend React", image: "/images/project3.jpg", link: "https://dreplica.netlify.app" },
     { name: "Project 4", description: "Description of project 4", image: "/images/project4.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
     { name: "Project 5", description: "Description of project 5", image: "/images/project5.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
