49fe780f6e86b68bde4c14d0783582550ab561a3
Olekszij <olekszijk@gmail.com>
2024-09-17 00:09:38 +0200

    new pic

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -9,8 +9,8 @@ const projects = [
     { name: "Vinted replica", description: "frontend/backend", image: "/images/project1.jpg", link: "https://vintversion.netlify.app/" },
     { name: "Marvel Comics Database", description: "Frontend React", image: "/images/project2.jpg", link: "https://jenemarrrrvel.netlify.app/" },
 
-    { name: "Project 4", description: "Description of project 4", image: "/images/project4.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
-    { name: "Project 5", description: "Description of project 5", image: "/images/project5.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
+    { name: "Car Rental Reservation Application", description: "Frontend React/Backend Express", image: "/images/project4.jpg", link: "https://rentauto.netlify.app" },
+    { name: "Burger King replica", description: "Frontend/backend ask me for details", image: "/images/project5.jpg", link: "#" },
     { name: "Project 6", description: "Description of project 6", image: "/images/project6.jpg", link: "https://github.com/olegkondratenko/nextjs-portfolio" },
 ];
 
