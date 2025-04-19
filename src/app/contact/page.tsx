b8f2aeae4fb753a9a86b7b12bbacac46439c6eec
Olekszij <olekszijk@gmail.com>
2025-04-19 03:24:06 +0200

    deploy fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
             const response = await fetch('/', {
                 method: 'POST',
                 headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: new URLSearchParams(formData as any).toString(),
+                body: new URLSearchParams(Array.from(formData.entries()) as [string, string][]).toString(),
             });
 
             if (response.ok) {
